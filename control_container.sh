c64b8950f5cefdee1e7c818649e77057c99807b3
shogle <seyoumoon@gmail.com>
2023-10-04 17:24:10 +0900

    final

--- control_container.sh
+++ control_container.sh
@@ -2,10 +2,12 @@
 
 echo "## control Beharbor-Server Container"
 
-echo "start / stop"
+echo "Write a your class name..."
+read class
+echo "start / stop / rm"
 read type
 
 while read line || [ -n "$line" ] ; do
-    echo "$type $line 's Ubuntu Container"
-    docker $type $line
-done < ps
+    echo "$type $class$line 's Ubuntu Container"
+    docker $type $class$line
+done < id
